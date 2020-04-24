efd66f557062eee45506c9e76ed2b3d0b7b243f9
Ichurin <h1took2@gmail.com>
2020-04-25 02:32:59 +0300

    Homework Lesson1

---
+++ ex1.3.py
@@ -0,0 +1,8 @@
+Figure = input("Введите число:")
+Figure = int(Figure)
+n = Figure
+n2 = Figure * 10 + Figure
+n3 = Figure * 100 + n2
+Amount = n + n2 + n3
+print("Сумма чисел {} + {} + {} = {}".format(n, n2, n3, Amount))
+
